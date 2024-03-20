4268b16d87a3594ea64d0ccf0f87d7966be952a3
turman <yuriy@Turman-2.local>
2024-03-20 14:01:48 +0000

    feat: CPP08 ex02

--- CPP08/ex02/MutantStack.h
+++ CPP08/ex02/MutantStack.h
@@ -1,6 +1,47 @@
 #ifndef MUTANTSTACK_H
 #define MUTANTSTACK_H
 
+#include <stack>
+#include <iterator>
+#include <iostream>
 
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+    MutantStack(){};
+    MutantStack(MutantStack const &other) { (void)other; };
+    MutantStack &operator=(MutantStack const &other)
+    {
+        (void)other;
+        return *this;
+    };
+    ~MutantStack(){};
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+    iterator begin()
+    {
+        return this->c.begin();
+    }
+
+    iterator end()
+    {
+        return this->c.end();
+    }
+
+    const_iterator begin() const
+    {
+        return this->c.begin();
+    }
+
+    const_iterator end() const
+    {
+        return this->c.end();
+    }
+
+private:
+};
 
 #endif
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -1,8 +1,18 @@
+
 #include "MutantStack.h"
+#include <list>
+
+template <typename T>
+void printContainer(T &container)
+{
+    for (typename T::iterator it = container.begin(); it != container.end(); it++)
+        std::cout << *it << std::endl;
+}
 
 int main()
 {
     MutantStack<int> mstack;
+
     mstack.push(5);
     mstack.push(17);
     std::cout << mstack.top() << std::endl;
@@ -17,11 +27,25 @@ int main()
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
+    std::cout << "here we begin the iteration\n";
     while (it != ite)
     {
         std::cout << *it << std::endl;
         ++it;
     }
+    std::cout << "here we end the iteration\n";
     std::stack<int> s(mstack);
-    return 0;
+
+    // Now with std::list
+    std::cout << "Test with std::list" << std::endl;
+    std::list<int> list;
+    list.push_back(5);
+    list.push_back(17);
+    list.pop_back();
+    list.push_back(3);
+    list.push_back(5);
+    list.push_back(737);
+    list.push_back(0);
+    printContainer(list);
+    return (0);
 }
